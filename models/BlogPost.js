1accf682e56f4d54692f053db942097e80e084a0
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-12-17 10:49:19 +0100

    datapost finished

---
+++ models/BlogPost.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({ //schema represents how a collection looks like on atles
+    title: String,
+    body:String,
+    datePosted: {/* can declare property type with an object like this beacuse we need 'default'*/
+        type: Date,
+        default: new Date()
+}
+});
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema); 
+//blogPost is a collection on mongo
+
+module.exports = BlogPost;
--- server.js
+++ server.js
@@ -2,20 +2,62 @@ const express = require("express");
 const app = express();
 const path = require('path'); 
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const BlogPost = require('./models/BlogPost.js')
+
+
+
+mongoose.connect('mongodb://localhost/mydatabase', {
+    useNewUrlParser: true
+})
 
 app.set('view engine', 'ejs')
 
+//bodyparser deprecated
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 app.use(express.static('public')); 
 
 
-app.get('/', (req,res) => {
+///////////////////////////////
+//CRUD ==> Create Read Update Delete
+
+app.get('/', async (req,res) => {
    // pre ejs res.sendFile(path.resolve(__dirname, 'index.html'))
-   res.render('index');
+   const blogposts = await BlogPost.find({})
+   res.render('index', {
+       blogposts: blogposts
+   });
+   console.log(blogposts)
+   console.log("blogposts")
+});
+
+
+// GET ONE 
+app.get('/post/:id', async (req, res) =>{
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
+        blogpost
+    })
 })
 
+// POST
+app.post('/posts/store', async (req, res)=>{
+    //console.log(req.body)
+    // model creates a new doc with browser data
+    await BlogPost.create(req.body)
+    res.redirect('/')
+});
+
+
+// pages routes 
 app.get('/about', (req,res) => {
     res.render('about');
+})
+app.get('/posts/new', (req, res)=>{
+    res.render('create')
 })
 
 // initializating the server
