7eb2f03f9ee46bfb154735d049820380a7670aca
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-12-29 20:57:59 +0100

    first page as all my positions

--- controllers/getPosts.js
+++ controllers/getPosts.js
@@ -1,14 +1,16 @@
 const BlogPost = require('../models/BlogPost.js')
 
-
-
 //getallposts
 module.exports = async (req,res) => {
-    const blogposts = await BlogPost.find({}).populate('userid')
-    console.log(req.session)
-    const sessionUserId = req.session.userId //user id for my posts
-    res.render('index', {
-        blogposts,
-        sessionUserId
-    });
+    if(req.session.userId){
+        const sessionUserId = req.session.userId //user id 
+        const blogposts = await BlogPost.find({userid: sessionUserId}).populate('userid')
+        console.log(req.session)
+       
+        res.render('index', {
+            blogposts,
+            sessionUserId
+        });
+    }
+    res.redirect('/auth/login')
 }
