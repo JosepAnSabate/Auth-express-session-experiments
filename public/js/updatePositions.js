81c00c3c61e34d30fb86f0d475c10ac6b37a7db8
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-12-31 20:49:00 +0100

    put acabat

--- public/js/updatePositions.js
+++ public/js/updatePositions.js
@@ -1,27 +1,22 @@
-
- function editButton () {
-    let id = document.getElementById("name");
-    let updatetitle = document.getElementById("new-name");
-    let updatebody = document.getElementById("new-description");
+const updateForm = document.querySelector('#edit'); //edit is id form
+ 
+updateForm.addEventListener('submit', async(e) => {
+  e.preventDefault() // no refresca la pagina
   
-    let newData = {
-      "title": updatetitle.value,
-      "body": updatebody.value,
-      "id": id.value
-    }
+  let updatetitle = updateForm.elements['new-name'].value
+  //let updatebody = document.querySelector('#new-description').value
+  // or
+  let updatebody = document.getElementById("new-description").value;
+  let id = edit.dataset.id //data-id
+  //console.log(updatetitle, updatebody, id)
   
-    try {
-      const response = fetch(`http://localhost:5000/post/update/${id}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" }, //http header fields => application/json if the http response is json data   https://www.youtube.com/watch?v=iYM2zFP3Zn0
-        body: JSON.stringify(newData)//{name, description, geom}
-      });
-      console.log(response);
-      window.location = "/"; // refresh and show the changes
-    }  catch (err) {
-      console.error(err.message);
-    }
-  }
+   const data = await  fetch (`/positions/update/${id}`,{
+      method: 'PUT',
+      headers: { "Content-Type": "application/json" }, //http header fields => application/json if the http response is json data, per forms es diferent
+      body: JSON.stringify({title: updatetitle, body: updatebody})
+    })
+    window.location.reload();
+})
 
   function openForm() {
     document.getElementById("myFormEdit").style.display = "block";
